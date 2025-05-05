8cedb5a9a4e0a719ea18efd8e47d0da184db16c3
AgoCodeBro <agoimbimboparra@gmail.com>
2025-05-05 17:40:52 +0000

    Finished pieces.py and added move method to board manager

--- src/board_manager.py
+++ src/board_manager.py
@@ -1,6 +1,74 @@
+from pieces import Color, Piece
 class BoardManager():
-    """Represents the board and controls the legal moves"""
+    """Represents the board and controls the legal moves
+
+    Attributes:
+        board (list[list[None|pieces]]): and 8 X 8 2d list that represents the chess board (0,0) is a1 and (7,7)
+            is h8. The first number is the file and the second the rank
+        to_move (bool): Flag designating whos turn it is
+        en_passant (bool): Flag that says if enpassant is playable this turn
+        en_passant_pos (tuple[int, int] | None): Holds postion of pawn capturable with en passant or None if there 
+            is no such pawn
+    """
 
     def __init__(self):
-        self.board = [None] * 64
+        """Creates and instance of the board managers"""
+        self.board = [[None] * 8 for _ in range(8)]
+        self.to_move = Color.WHITE
+        self.en_passant = False
+        self.en_passant_pos = None
 
+    def move(self, start: tuple[int, int], end: tuple[int, int]) -> None:
+        piece = self.board[start[0]][start[1]]
+
+        if piece is not None:
+            legal_moves = piece.generate_valid_moves(start, self.board, self.en_passant, self.en_passant_pos)
+
+            if end in legal_moves:
+                self.board[end[0]][end[1]] = piece
+                self.board[start[0]][start[1]] = None
+
+                self.check_en_passant(piece, start, end)
+
+                self.check_promotion(piece, end)
+                          
+            
+            else:
+                raise ValueError("Not a legal move")
+            
+        else: 
+            raise ValueError("No piece selected")
+        
+    def check_en_passant(self, piece: Piece, start: tuple[int, int], end: tuple[int, int]) -> None:
+        """Checks if en passant is playable on the board next move and sets self.en_passant, and self.en_passant_pos to the correct values
+
+        Args:
+            piece (Piece): piece that was just moved
+            start (tuple[int, int]): Square the piece started on
+            end (tuple[int, int]): Square the piece ended on
+        """
+        if type(piece).__Name__ == "Pawn":
+            if start[1] - end[1] == 2 or start[1] - end[1] == -2:
+                if end[0] + 1 <= 7:
+                    square_to_the_right = self.board[end[0] + 1][end[1]]
+
+                    if type(square_to_the_right).__Name__ == "Pawn" and square_to_the_right.color != piece.color:
+                        en_passant = True
+                        en_passant_pos = end
+                        return
+
+                if end[0] - 1 >= 0:
+                    square_to_the_left = self.board[end[0] - 1][end[1]]
+
+                    if type(square_to_the_left).__Name__ == "Pawn" and square_to_the_left.color != piece.color:
+                        en_passant = True
+                        en_passant_pos = end
+                        return
+                    
+        en_passant = False
+        en_passant_pos = None
+
+    def check_promotion(self, piece: Piece, end: tuple[int, int]) -> None:
+        if type(piece).__Name__ == "Pawn" and (end[1] == 0 or end[1] == 1):
+            print("this pawn should promote")
+        
--- src/pieces.py
+++ src/pieces.py
@@ -26,8 +26,13 @@ class Piece(ABC):
         self.color = color
 
     @abstractmethod
-    def generate_valid_moves(self, board: BoardManager) -> list:
-        """returns a list of all the valid moves the piece can make"""
+    def generate_valid_moves(self, position: tuple[int,int], board: list[list['Piece' | None]], en_passant: bool = False) -> list[tuple[int, int]]:
+        """returns a list of all the valid moves the piece can make
+        Args:
+            position (tuple[int, int]): A tuple contating 2 ints that give where on the board this piece is.
+            board (list[list['Piece' | None]]): A representation of the board itself.
+            en_passant (bool): Flag that says if there is en_passant possible on this move
+        """
         pass
 
     @property
@@ -42,13 +47,84 @@ class Piece(ABC):
         """Returns the value of the piece"""
         pass
 
+    def __repr__(self):
+        return f"{self.color.value}{self.symbol}"
+
 
 class Pawn(Piece):
     """Class representing a pawn"""
 
     def __init__(self, color: Color) -> None:
         """Creates an instance of a Pawn of the given color
+
+        Args:
+            color (Color):
         """
         super().__init__(color)
+        self.has_moved = False
+        self._symbol = 'p'
+        self._value = 1
+
+    def generate_valid_moves(self,
+                             position: tuple[int, int],
+                             board: list[list['Piece' | None]],
+                             en_passant: bool = False,
+                             en_passant_pos: tuple[int,int] | None = None
+                             ) -> list:
+        """Generates a list of all the valid moves this piece can make
+        Args:
+            position (tuple[int, int]): A tuple contating 2 ints that give where on the board this piece is.
+            board (list[list['Piece' | None]]): A representation of the board itself.
+            en_passant (bool): Flag that says if there is en_passant possible on this move
+            en_passant_pos (tuple[int,int] | None): Holds the square of the pawn available for enpassant and None if there is no enpassant
+        """
+        if self.color == Color.WHITE:
+            move_direction = 1
+
+        else:
+            move_direction = -1
+
+        # Validate positon is on the board
+        for i in position:
+            if i > 7 or i < 0:
+                raise ValueError("Piece's position is off of the board")
+            
+            elif i == 7 or i == 0:
+                raise ValueError("Pawns cant be at the end of the board. Promotion did not occur")
+            
+        # Check forward movement
+        moves = []
+        next_square = board[position[0]][position[1] + move_direction]
+
+        if next_square is None:
+            moves.append((position[0], position[1] + move_direction))
+
+            next_square = board[position[0]][position[1] + (move_direction * 2)]
+
+            if next_square is None and not self.has_moved:
+                moves.append((position[0], position[1] + (move_direction * 2)))
+
+        # Check takes
+        if position[0] + 1  <= 7:
+            next_square = board[position[0] + 1][position[1] + move_direction]
+
+            if next_square is not None and next_square.color != self.color:
+                moves.append((position[0] + 1, position[1] + move_direction))
+
+        if position[0] - 1  <= 7:
+            next_square = board[position[0] - 1][position[1] + move_direction]
+
+            if next_square is not None and next_square.color != self.color:
+                moves.append((position[0] - 1, position[1] + move_direction))
+
+        # Check en_passant
+        if en_passant:
+            if en_passant_pos[0] + 1 == position[0] or en_passant_pos[0] - 1 == position[0]:
+                if en_passant_pos[1] == position[1]:
+                    moves.append((en_passant_pos[0], en_passant_pos[1] + move_direction))
+
+
+        return moves
+
 
 
